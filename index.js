420f242fc5fd4a14bdcb8021847a8db1fd95e5dd
Bonyonte <godfrey.magogo@student.moringaschool.comgit config --global user.name Bonyontegit branch -m maingit branch -M maingit remote add origin git@github.com:Bonyonte/ip_1.gitgit push -u origin maingit config --global user.email godfrey.magogo@student.moringaschool.com>
2022-06-08 18:04:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result
+  if (ride<=400){
+    result = 'This one is on me!';
+  }
+  else if (ride > 2000 && ride <2500){
+    result = 'I will gladly take your thirty bucks.';
+  }
+  else if (ride >2500){
+    result='No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+let result;
+city === "NYC" ? (result = 'Ok, sounds good.') : (result = 'No go.');
+
+return result
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  let result;
+switch (generous) {
+  case 'generous':
+    result = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    result = 'Thank you.';
+    break;
+  case 'thanks for everything':
+    result = 'Bye.';
+}
+return result;
 }
